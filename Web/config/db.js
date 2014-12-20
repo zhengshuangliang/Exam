bb38d18c555812f9194efcacb40f558dc345d4e8
zsloffice <liang4571231@163.com>
2014-12-20 22:41:35 +0800

    t

--- Web/config/db.js
+++ Web/config/db.js
@@ -1,6 +1,6 @@
 exports.AuthDBHost = 'localhost';
 exports.AuthDBUser = 'root';
-exports.AuthDBPass = '123456root1';
+exports.AuthDBPass = '123456root12';
 exports.AuthDBName = 'exercises';
 exports.AuthDBPort = 3306;
 
